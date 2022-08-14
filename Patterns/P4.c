9deb457e9e506fd7da20c41bb7327c4627f7d899
Aditya Seth <adityaseth.cse@gmail.com>
2022-08-14 18:23:59 +0530

    P4_Code

---
+++ Patterns/P4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void pattern(int);
+void pattern(int n)
+{
+    int i,j;
+    printf("\nRequired pattern upto %d rows: \n",n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        printf("%d ",i);
+        printf("\n");
+    }
+}
+void main()
+{
+    int rows;
+    printf("\nEnter the number of rows: ");
+    scanf("%d",&rows);
+    pattern(rows);
+}
