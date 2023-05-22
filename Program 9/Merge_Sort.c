2621bffc7dd5c7bb4e4d679f6d0df884da5540cf
Aditya Seth <adityaseth.cse@gmail.com>
2023-05-22 21:34:09 +0530

    Update Merge_Sort.c

--- Program 9/Merge_Sort.c
+++ Program 9/Merge_Sort.c
@@ -1,83 +1,84 @@
 #include <stdio.h>
- 
-void mergeSort(int [], int, int, int);
-void partition(int [],int, int);
+
+void merge(int [], int, int, int);
+void mergeSort(int [], int, int);
  
 int main()
 {
     int list[50];
     int i, size;
  
-    printf("Enter total number of elements: ");
+    printf("Enter the total number of elements: ");
     scanf("%d", &size);
     printf("Enter the elements:\n");
     for(i = 0; i < size; i++)
     {
-         scanf("%d", &list[i]);
+        scanf("%d", &list[i]);
     }
-    partition(list, 0, size - 1);
+    mergeSort(list, 0, size - 1);
     printf("After merge sort:\n");
-    for(i = 0;i < size; i++)
+    for(i = 0; i < size; i++)
     {
-         printf("%d   ",list[i]);
+        printf("%d   ", list[i]);
     }
  
    return 0;
 }
- 
-void partition(int list[],int low,int high)
+
+void merge(int list[], int low, int mid, int high)
 {
-    int mid;
- 
-    if(low < high)
+    int leftSize = mid - low + 1;
+    int rightSize = high - mid;
+
+    int leftArray[leftSize], rightArray[rightSize];
+
+    for (int i = 0; i < leftSize; i++)
+        leftArray[i] = list[low + i];
+    
+    for (int j = 0; j < rightSize; j++)
+        rightArray[j] = list[mid + 1 + j];
+
+    int i = 0, j = 0, k = low;
+
+    while (i < leftSize && j < rightSize)
     {
-        mid = (low + high) / 2;
-        partition(list, low, mid);
-        partition(list, mid + 1, high);
-        mergeSort(list, low, mid, high);
-    }
-}
- 
-void mergeSort(int list[],int low,int mid,int high)
-{
-    int i, mi, k, lo, temp[50];
- 
-    lo = low;
-    i = low;
-    mi = mid + 1;
-    while ((lo <= mid) && (mi <= high))
-    {
-        if (list[lo] <= list[mi])
+        if (leftArray[i] <= rightArray[j])
         {
-            temp[i] = list[lo];
-            lo++;
+            list[k] = leftArray[i];
+            i++;
         }
         else
         {
-            temp[i] = list[mi];
-            mi++;
+            list[k] = rightArray[j];
+            j++;
         }
+        k++;
+    }
+
+    while (i < leftSize)
+    {
+        list[k] = leftArray[i];
         i++;
+        k++;
     }
-    if (lo > mid)
+
+    while (j < rightSize)
     {
-        for (k = mi; k <= high; k++)
-        {
-            temp[i] = list[k];
-            i++;
-        }
+        list[k] = rightArray[j];
+        j++;
+        k++;
     }
-    else
+}
+
+void mergeSort(int list[], int low, int high)
+{
+    if (low < high)
     {
-        for (k = lo; k <= mid; k++)
-        {
-             temp[i] = list[k];
-             i++;
-        }
-    }
- 
-    for (k = low; k <= high; k++)
-    {
-        list[k] = temp[k];
+        int mid = low + (high - low) / 2;
+
+        mergeSort(list, low, mid);
+        mergeSort(list, mid + 1, high);
+
+        merge(list, low, mid, high);
     }
 }
