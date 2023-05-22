e3d56a55871f23e978c23db7a937ce117dd73eb5
Aditya Seth <adityaseth.cse@gmail.com>
2023-05-22 22:12:08 +0530

    Update BubbleSort.c

--- Program 9/BubbleSort.c
+++ Program 9/BubbleSort.c
@@ -1,25 +1,42 @@
-// to perform sorting on an user-input array using Bubble-sort technique
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX 100
+
 void input_elements(int *, int);
 void display(int *, int);
 void bubble_sort(int *, int);
-void main()
+
+int main()
 {
     int *a, N;
+    
+    // Prompt the user to enter the number of elements
     printf("\n Enter the number of elements: ");
     scanf("%d", &N);
+    
+    // Dynamically allocate memory for the array based on the number of elements
     a = (int *)malloc(sizeof(int) * N);
+
+    // Call the function to input the elements from the user
     input_elements(a, N);
+
+    // Display the elements before sorting
     printf("\n Elements before sorting: ");
     display(a, N);
+
+    // Perform bubble sort to sort the elements
     bubble_sort(a, N);
+
+    // Display the elements after sorting
     printf("\n Elements after sorting: ");
     display(a, N);
+
     getchar();
     printf("\n");
+    return 0;
 }
+
+// Function to input the elements from the user
 void input_elements(int *x, int n)
 {
     int i;
@@ -30,6 +47,8 @@ void input_elements(int *x, int n)
         scanf("%d", x + i);
     }
 }
+
+// Function to display the elements of the array
 void display(int *x, int n)
 {
     int i;
@@ -37,11 +56,20 @@ void display(int *x, int n)
     for (i = 0; i < n; i++)
         printf("\t %d", x[i]);
 }
+
+// Function to perform bubble sort on the array
 void bubble_sort(int *x, int n)
 {
     int i, j, t, flag;
+    
+    // Iterate over the array elements
     for (i = 0, flag = 1; i < n - 1 && flag; i++)
-        for (j = 0, flag = 0; j < n - i - 1; j++)
+    {
+        flag = 0;
+        
+        // Compare adjacent elements and swap if necessary
+        for (j = 0; j < n - i - 1; j++)
+        {
             if (x[j] > x[j + 1])
             {
                 flag = 1;
@@ -49,4 +77,6 @@ void bubble_sort(int *x, int n)
                 x[j] = x[j + 1];
                 x[j + 1] = t;
             }
+        }
+    }
 }
