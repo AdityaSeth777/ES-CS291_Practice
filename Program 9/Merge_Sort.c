6eb5e770f7cee355faff8123e358fc426394e592
Aditya Seth <adityaseth.cse@gmail.com>
2023-05-22 22:05:53 +0530

    Update Merge_Sort.c

--- Program 9/Merge_Sort.c
+++ Program 9/Merge_Sort.c
@@ -2,12 +2,12 @@
 
 void merge(int [], int, int, int);
 void mergeSort(int [], int, int);
- 
+
 int main()
 {
     int list[50];
     int i, size;
- 
+
     printf("Enter the total number of elements: ");
     scanf("%d", &size);
     printf("Enter the elements:\n");
@@ -15,46 +15,59 @@ int main()
     {
         scanf("%d", &list[i]);
     }
+
     mergeSort(list, 0, size - 1);
+
     printf("After merge sort:\n");
     for(i = 0; i < size; i++)
     {
         printf("%d   ", list[i]);
     }
- 
-   return 0;
+
+    return 0;
 }
 
+// Function to merge two sorted subarrays
 void merge(int list[], int low, int mid, int high)
 {
-    int leftSize = mid - low + 1;
-    int rightSize = high - mid;
+    int leftSize = mid - low + 1;       // Size of the left subarray
+    int rightSize = high - mid;         // Size of the right subarray
 
-    int leftArray[leftSize], rightArray[rightSize];
+    int leftArray[leftSize], rightArray[rightSize];   // Temporary arrays to hold the subarrays
 
+    // Copy elements from list to leftArray
     for (int i = 0; i < leftSize; i++)
+    {
         leftArray[i] = list[low + i];
-    
+    }
+
+    // Copy elements from list to rightArray
     for (int j = 0; j < rightSize; j++)
+    {
         rightArray[j] = list[mid + 1 + j];
+    }
 
-    int i = 0, j = 0, k = low;
+    int i = 0;     // Index for leftArray
+    int j = 0;     // Index for rightArray
+    int k = low;   // Index for merged list
 
+    // Merge the two sorted subarrays into list in ascending order
     while (i < leftSize && j < rightSize)
     {
         if (leftArray[i] <= rightArray[j])
         {
-            list[k] = leftArray[i];
+            list[k] = leftArray[i];    // Place the smaller element from leftArray into list
             i++;
         }
         else
         {
-            list[k] = rightArray[j];
+            list[k] = rightArray[j];   // Place the smaller element from rightArray into list
             j++;
         }
         k++;
     }
 
+    // Copy any remaining elements from leftArray to list
     while (i < leftSize)
     {
         list[k] = leftArray[i];
@@ -62,6 +75,7 @@ void merge(int list[], int low, int mid, int high)
         k++;
     }
 
+    // Copy any remaining elements from rightArray to list
     while (j < rightSize)
     {
         list[k] = rightArray[j];
@@ -70,15 +84,16 @@ void merge(int list[], int low, int mid, int high)
     }
 }
 
+// Function to perform merge sort
 void mergeSort(int list[], int low, int high)
 {
     if (low < high)
     {
-        int mid = low + (high - low) / 2;
+        int mid = low + (high - low) / 2;   // Calculate the middle index
 
-        mergeSort(list, low, mid);
-        mergeSort(list, mid + 1, high);
+        mergeSort(list, low, mid);          // Recursively sort the left subarray
+        mergeSort(list, mid + 1, high);     // Recursively sort the right subarray
 
-        merge(list, low, mid, high);
+        merge(list, low, mid, high);        // Merge the two sorted subarrays
     }
 }
